ed8fa0d3ed993766e50800948ea3fa1da04f653f
Abhiraj412 <abhangparth@gmail.com>
2024-06-04 17:57:53 +0530

    Server created with hello world msg

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function handleFirstRequest(req, res)
+{
+    res.send('Hello World!')
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/', handleFirstRequest)
+
+function started ()
+{
+    console.log(`Example app listening on port ${port}`)
+}
+
+app.listen(port, started)
 
 //calculate the sum from 1 to 100
 function calculateSum(counter) 
@@ -25,24 +31,29 @@ var calculatedSum= calculateSum(100)
 console.log(calculatedSum);
 
 //reading data from file through two different ways.
-const fs = require('fs');
-//method 1:
-function readFileData(err, data) 
-{
-    if (err)
-    {
-        console.log("Error reading from the file: ", err);
-    }
-    else 
-    {
-        console.log(data.toString())
-    }
-}
+// const fs = require('fs');
 
-fs.readFile("demo.txt", readFileData);
+
+// //method 1:
+// function readFileData(err, data) 
+// {
+//     if (err)
+//     {
+//         console.log("Error reading from the file: ", err);
+//     }
+//     else 
+//     {
+//         console.log(data.toString())
+//     }
+// }
+
+// fs.readFile("demo.txt", readFileData);
+
+
 //method 2:
-var data = fs.readFileSync('demo.txt');
-console.log(data.toString());
+// var data = fs.readFileSync('demo.txt');
+// console.log(data.toString());
  
+
 //write data in the file.
-var input= fs.writeFileSync("write.txt", "This is the input being encoded into the file.")
+// var input= fs.writeFileSync("write.txt", "This is the input being encoded into the file.")
