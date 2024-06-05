21f1a0d92baecf36d7bc2dded8a197ae8d0aebad
Abhiraj412 <abhangparth@gmail.com>
2024-06-05 16:02:55 +0530

    algorithm deployed on the server

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-function handleFirstRequest(req, res)
-{
-    res.send('Hello World!')
+function handleFirstRequest(req, res) {
+  var calculatedSum = calculateSum(100);
+  console.log(calculatedSum);
+  var answer="The sum calculated by the algorithm is " + calculatedSum;
+  res.send(answer);
 }
 
-app.get('/', handleFirstRequest)
+app.get("/", handleFirstRequest);
 
-function started ()
-{
-    console.log(`Example app listening on port ${port}`)
+function started() {
+  console.log(`Example app listening on port ${port}`);
 }
 
-app.listen(port, started)
+app.listen(port, started);
 
 //calculate the sum from 1 to 100
-function calculateSum(counter) 
-{
-    var sum=0;
-    for (var i=0; i<counter; i++)
-    {
-        sum = sum+i;
-    }
-    return sum;
+function calculateSum(counter) {
+  var sum = 0;
+  for (var i = 0; i < counter; i++) {
+    sum = sum + i;
+  }
+  return sum;
 }
-
-var calculatedSum= calculateSum(100)
-console.log(calculatedSum);
 
 //reading data from file through two different ways.
 // const fs = require('fs');
 
-
 // //method 1:
-// function readFileData(err, data) 
+// function readFileData(err, data)
 // {
 //     if (err)
 //     {
 //         console.log("Error reading from the file: ", err);
 //     }
-//     else 
+//     else
 //     {
 //         console.log(data.toString())
 //     }
@@ -49,11 +44,9 @@ console.log(calculatedSum);
 
 // fs.readFile("demo.txt", readFileData);
 
-
 //method 2:
 // var data = fs.readFileSync('demo.txt');
 // console.log(data.toString());
- 
 
 //write data in the file.
 // var input= fs.writeFileSync("write.txt", "This is the input being encoded into the file.")
