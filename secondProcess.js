80700c8575775038b992a4340b7c719aa7ec3d69
Abhiraj412 <abhangparth@gmail.com>
2024-06-08 22:02:15 +0530

    added proper comments

--- secondProcess.js
+++ secondProcess.js
@@ -9,7 +9,7 @@ function logResponseBody(jsonBody)
 
 function callBackFn(result)
 {
-    result.json().then(logResponseBody)
+    result.json().then(logResponseBody)  //extracted json from result and then sent it to logResponseBody which means basicaclly called another asyncronus fn in a asynchronus function.
 } 
 
-fetch("http://localhost:3000/handleSum?counter=10", sendObj).then(callBackFn);
+fetch("http://localhost:3000/handleSum?counter=10", sendObj).then(callBackFn);  //fetch is a library that helps u send request to the browser by taking a url from the user.
