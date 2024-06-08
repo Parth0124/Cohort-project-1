67d95081150a15b1945460ee6bfb9483e676bb23
Abhiraj412 <abhangparth@gmail.com>
2024-06-08 21:55:55 +0530

    body extracted from the browser by the second nodejs process

--- secondProcess.js
+++ secondProcess.js
@@ -2,9 +2,14 @@ var sendObj = {
     method: "GET"
 }
 
+function logResponseBody(jsonBody)
+{
+    console.log(jsonBody)
+}
+
 function callBackFn(result)
 {
-    console.log(result)
+    result.json().then(logResponseBody)
 } 
 
 fetch("http://localhost:3000/handleSum?counter=10", sendObj).then(callBackFn);
